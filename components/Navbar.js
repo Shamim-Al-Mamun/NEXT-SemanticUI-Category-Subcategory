3ae7198234bef656b6b5588823c5ad271843c5ee
Mamun <shamimalmamun.aiub@gmail.com>
2022-10-17 12:58:18 +0600

    nav added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-// import Link from 'next/link';
+import Link from 'next/link';
 import style from './css/style.module.css';
 import { Menu } from 'semantic-ui-react'
 import { useState } from 'react';
@@ -9,26 +9,31 @@ const Navbar = () => {
     const [activeItem, setactiveItem] = useState("All Category");
 
     return (
+        <>
+        
         <nav className={style.navbar}>
-            {/* <Link href="/new">
+            <Link href="/new">
                 <a className={style.create}>Add catagory</a>
             </Link>
             <Link href="/">
                 <a className={style.create}>All Catagories</a>
-            </Link> */}
+            </Link>
+            </nav>
+                  <nav className={style.navbar}>
         <Menu color='blue' widths={2}>
             <Menu.Item
             name='New Category'
             active={activeItem === 'New Category'}
-            onClick={(e, { name }) => {setactiveItem(name); router.push('/new') }}
+            onClick={(e, { name }) => {setactiveItem(name); router.push('https://next-category-subcategory-cnj96ktno-shamimalmamunaiub-gmailcom.vercel.app/new'); }}
             />
             <Menu.Item
             name='All Category'
             active={activeItem === 'All Category'}
-            onClick={(e, { name }) => {setactiveItem(name); router.push('/') }}
+            onClick={(e, { name }) => {setactiveItem(name); router.push('https://next-category-subcategory-cnj96ktno-shamimalmamunaiub-gmailcom.vercel.app/') }}
             />
       </Menu>
         </nav>
+        </>
 
     );
 }
